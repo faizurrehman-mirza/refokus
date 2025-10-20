f729210a9226eac17a38d777d32c440115f93139
faizurrehman <mirzafaiz506@gmail.com>
2025-10-20 12:39:52 +0530

    added button animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import { motion } from "motion/react";
+import React from "react";
 import { PiArrowBendDownRight } from "react-icons/pi";
 
-const Button = ({title ='Get Started'}) => {
+const Button = ({ title = "Get Started" }) => {
   return (
-    <div className=' flex items-center justify-between w-36 bg-zinc-100 text-black px-4 py-1 rounded-full '><span className='font-regular'>{title}</span>
-    <PiArrowBendDownRight/>
-    </div>
-  )
-}
+    <motion.div
+      whileHover={{ scale: 1.1, transition: { duration: 0.1 } }}
+      transition={{duration:0.5}}
+      className=" flex items-center justify-between w-36 bg-zinc-100 text-black px-4 py-1 rounded-full "
+    >
+      <span className="font-regular">{title}</span>
+      <PiArrowBendDownRight />
+    </motion.div>
+  );
+};
 
-export default Button
+export default Button;
